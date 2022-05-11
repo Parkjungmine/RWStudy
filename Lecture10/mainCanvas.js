480a4061b081c7cbdab0220139f08ca96b9d47f2
Parkjungmine <89765916+Parkjungmine@users.noreply.github.com>
2022-05-11 09:34:04 +0900

    Add files via upload

--- Lecture10/mainCanvas.js
+++ Lecture10/mainCanvas.js
@@ -7,38 +7,63 @@ var context = canvas.getContext("2d");
 // 이 openGL context를 자료구조로 갖고있는데 skia context
 // 이 skia context를 갖고있는게 canvas context이다.
 
-class Player{
-    constructor(pos_x,pos_y,radius,color){
-        this.pos_x = pos_x;
-        this.pos_y = pos_y;
+var canvas = document.getElementById('game');
+var context = canvas.getContext('2d');
+
+class gameManager{
+
+}
+
+
+
+class player{
+    constructor(x,y,radius,color)
+    {
+        this.pos_x = x;
+        this.pos_y = y;
         this.radius = radius;
         this.color = color;
     }
 
     draw()
     {
+        context.arc(this.pos_x,this.pos_y,this.radius,0,2*Math.PI)
+        context.fillStyle = this.color;
+        context.fill()
+    }
+
+}
+
+class bullet{
+    constructor(position_x,position_y,radius,color){
+        this.position_x = position_x;
+        this.position_y = position_y;
+        this.radius = radius;
+        this.color = color;
+    }
+    draw()
+    {
         context.beginPath();
-        context.arc(this.pos_x,this.pos_y,this.radius, 0, 2*Math.PI);
+        context.arc(this.position_x, this.position_y, this.radius, 0, 2*Math.PI);
         context.fillStyle = this.color;
         context.fill();
+        context.closePath();
     }
 }
 
-var char_player = new Player(100,100,20,'skyblue');
+var char_player = new player(100,100,20,'skyblue');
 char_player.draw();
 
 var bullets = [];
 
-canvas.onclick = function(event)
-{
+canvas.onclick = function(event){
+
     let clickpos_x = event.clientX -context.canvas.offsetLeft;
     let clickpos_y = event.clientY -context.canvas.offsetTop;
 
     let bullets = new bullet(clickpos_x,clickpos_y,5,5,'pink');
     bullets.draw();
-}
-
-
+};
 //class Bullet{
   //  constructor(pos_x,pos_y,width, height,color){
     //    this.pos_x = pos_x;
