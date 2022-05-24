43356a85ca719d39a8d1922748879c4b622e4b42
Parkjungmine <89765916+Parkjungmine@users.noreply.github.com>
2022-05-25 01:07:19 +0900

    Add files via upload

---
+++ 기말 과제 오류/GameManager.js
@@ -0,0 +1,140 @@
+const canvas = document.
+    querySelector('canvas')
+    const c = canvas.getContext('2d')
+
+    canvas.width = innerWidth
+    canvas.height = innerHeight
+
+    const scoreEl = document.querySelector('#scoreEl')
+    console.log(scoreEl)
+   
+    //import GameManager form './player.js';
+
+   const x = canvas.width / 2
+   const y = canvas.height / 2
+
+
+    const player = new  Player(x,y, 30, 'white')
+    const projectile = []
+    const enemies = []
+
+    function spawnEnemies() {
+        setInterval(() => {
+            const radius = Math.random() * (30 -4 ) + 4
+            
+            let x
+            let y 
+
+            if(Math.random()< 0.5) {
+                x=Math.random() < 0.5 ? 0 -radius: canvas.
+                width + radius
+                y= Math.random() * canvas.height
+            } else{
+                x = Math.random() * canvas.width
+                y= Math.random() < 0.5 ? 0 -radius: canvas.
+                height + radius
+            }
+            const color = 'hsl(0, 50%, 50%)'
+
+            const angle = Math.atan2(canvas.height /2 - y,  canvas.width /2 - x)
+                const velocity = {
+                    x: Math.cos(angle),
+                    y: Math.sin(angle)
+                }
+            enemies.push(new Enemy(x, y, radius, color,
+                velocity))
+
+                console.log(enemies)
+        }, 1000)
+    }
+
+   
+    let animationId
+    let score = 0
+
+    function animate() {
+        animationId = requestAnimationFrame(animate)
+        c.fillStyle = 'rgba(0, 0, 0, 0.1)'
+        c.fillRect(0, 0, canvas.width, canvas.height)
+        player.draw()
+        projectile.forEach((projectile,index)=> {
+            projectile.update()
+
+        
+            if (projectile.x + projectile.radius < 0 ||
+                projectile.x - projectile.radius > canvas.width ||
+                projectile.y + projectile.radius < 0 ||
+                projectile.y - projectile.radius > canvas.height
+                ) {
+                setTimeout(()=> {
+                    projectile.splice(index, 1)
+                }, 0)
+            }
+        })
+        
+        enemies.forEach((enemy, index) => {
+            enemy.update()
+
+            const dist =  Math.hypot(player.x - enemy.x,
+                player.y - enemy.y)
+                //게임 끝
+                if (dist - enemy.radius - player.radius < 1 ){
+                    cancelAnimationFrame(animationId)
+                }
+
+            projectile.forEach((projectile, projectileIndex)=>{
+               const dist =  Math.hypot(projectile.x - enemy.x,
+                     projectile.y - enemy.y)
+                    // when projectiles touch enemy
+                    if (dist - enemy.radius - projectile.radius < 1)
+                    {
+
+                        
+
+                        if (enemy.radius - 10 > 10) {
+
+                            // score
+                        score += 100
+                        scoreEl.innerHTML = score
+                        console.log(score)
+                            enemy.radius -= 10
+                            setTimeout(() => {
+                                projectile.splice(projectileIndex , 1)
+                            }, 0)
+
+                        } else {
+                            // removescore
+                        score += 250
+                        scoreEl.innerHTML = score
+                        console.log(score)
+                        setTimeout(() => {
+                            enemies.splice(index, 1)
+                            projectile.splice(projectileIndex , 1)
+                        }, 0)
+                    }
+                }
+            } );
+        })
+    }
+
+    addEventListener('click', (event)=> 
+    {
+        const angle = Math.atan2(
+            event.clientY - canvas.height /2,
+            event.clientX - canvas.width /2
+            )
+            const velocity = {
+                x: Math.cos(angle) * 5,
+                y: Math.sin(angle) * 5
+            }
+        projectile.push(
+            new Projectile(canvas.width / 2,
+            canvas.height / 2, 5 ,'white', velocity)
+        )
+    })
+        
+        
+    animate()
+    spawnEnemies()
+    
+    
